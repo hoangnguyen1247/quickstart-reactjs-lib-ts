7aa5c4f7b21d024fe2a38b2d16e9a3cd22ff6e16
Hoàng Ng <hoangnguyen1247@gmail.com>
2020-09-03 14:45:59 +0700

    create project

---
+++ es/Alert.js
@@ -0,0 +1,6 @@
+import React from "react";
+export function Alert() {
+  {
+    return /*#__PURE__*/React.createElement("div", null, "Hello");
+  }
+}
---
+++ src/Alert.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Alert() {{
+
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}}
