e149fee561ca419e6b0dda917ab7630759a23a0f
Hoàng Ng <hoangnguyen1247@gmail.com>
2020-09-08 14:41:15 +0700

    add type-check

--- webpack.docs.config.js
+++ webpack.docs.config.js
@@ -2,9 +2,9 @@
 const path = require('path');
 const webpack = require('webpack');
 const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const env = process.env.WEBPACK_BUILD || process.env.NODE_ENV || 'development';
 
